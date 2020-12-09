978cdb32fffc9dc660312d8305b8d60c73bf4289
venkateshkudikala <vkudikala95@gmail.com>
2020-12-10 00:37:55 +0530

    commit with sample code and packages created

---
+++ testingAutomation/src/main/java/com/demo/tests/HomeTest.java
@@ -0,0 +1,15 @@
+package com.demo.tests;
+
+import org.testng.annotations.Test;
+
+import com.demo.pages.HomePage;
+
+public class HomeTest {
+
+	HomePage homePage;
+	@Test
+   public void TestMethod() {
+	   System.out.println("get title");
+   }
+	
+}
